73a997240dffeec1989840d01ee540e6bfedcb3e
Magnus Westhofen <m.westhofen@12-05.de>
2020-09-22 00:42:45 +0200

    test

--- src/components/Chat/data.js
+++ src/components/Chat/data.js
@@ -15,7 +15,7 @@ const data = [
     slug: "bio",
     messages: [
       "My name is Magnus Westhofen and I'm from Solingen, Germany and I'm 30 years old.",
-      "I've been designing and developing stuff for the web since I'm 14. The first website I've done was for my CS 1.3 clan ;-)",
+      "I've been designing and developing stuff for the web since I'm 14. The first website I've done was for my CS clan ;-)",
       "I am working for a small agency shop in Solingen right now and my main focus is building cool frontends with React.",
     ],
     answers: [
@@ -27,7 +27,7 @@ const data = [
     slug: "tech",
     messages: [
       "I love building with React right now, but I don't shy away from vanilla CSS/HTML and Javascript if needed.",
-      "I really like using serverless/jamstacky backends like Firebase, but surely can spin up some basic node Backend stack if needed.",
+      "I really like using serverless/jamstacky backends like Firebase, but I can spin up some basic node Backend stack if needed.",
       "My guilty pleasure is developing with/for WordPress. Especially using it as headless CMS to power React Frontends.",
     ],
     answers: [{ label: "Cool, show me some projects", link: "/portfolio" }],
