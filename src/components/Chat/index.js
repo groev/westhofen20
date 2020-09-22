e6e335b8ed5a79a0bf414e599971e6f8e5c92206
Magnus Westhofen <magnuswesthofen@icloud.com>
2020-09-22 10:24:30 +0200

    refactoring styling

--- src/components/Chat/index.js
+++ src/components/Chat/index.js
@@ -1,93 +1,85 @@
-import React, { useEffect, useState } from "react";
-import "./Chat.scss";
-import { useParams } from "react-router-dom";
-import data from "./data";
-import Message from "./Message";
-import Answer from "./Answer";
-import Typing from "./Typing";
-import { Head } from "../../images";
-import { motion } from "framer-motion";
-import MessageSound from "../../sounds/message.mp3";
-import Sound from "react-sound";
+import React, { useEffect, useState } from 'react';
+import './Chat.scss';
+import { useParams } from 'react-router-dom';
+import data from './data';
+import Message from './Message';
+import Answer from './Answer';
+import Typing from './Typing';
+import { Head } from '../../images';
+import { motion } from 'framer-motion';
+
 export default function Chat() {
-  const [playing, setPlaying] = useState(false);
-  const { slug, status } = useParams();
-  const currentDialog = data.find((dialog) => dialog.slug === slug) || data[0];
-  const [messages, setMessages] = useState([]);
-  const [completed, setCompleted] = useState(false);
+	const { slug, status } = useParams();
+	const currentDialog = data.find((dialog) => dialog.slug === slug) || data[0];
+	const [messages, setMessages] = useState([]);
+	const [completed, setCompleted] = useState(false);
 
-  useEffect(() => {
-    setMessages([]);
-    setCompleted(false);
-    if (status !== "completed") {
-      currentDialog.messages &&
-        currentDialog.messages.forEach((message, idx) => {
-          setTimeout(function () {
-            setMessages((prevMessages) => [...prevMessages, message]);
-            setPlaying("PLAYING");
-            if (idx + 1 === currentDialog.messages.length) {
-              setTimeout(function () {
-                setCompleted(true);
-              }, 1500);
-            }
-          }, idx * 1500);
-        });
-    } else {
-      setMessages(currentDialog.messages);
-      setCompleted(true);
-    }
-  }, [slug, status]);
+	useEffect(() => {
+		setMessages([]);
+		setCompleted(false);
+		if (status !== 'completed') {
+			currentDialog.messages &&
+				currentDialog.messages.forEach((message, idx) => {
+					setTimeout(function () {
+						setMessages((prevMessages) => [...prevMessages, message]);
+						if (idx + 1 === currentDialog.messages.length) {
+							setTimeout(function () {
+								setCompleted(true);
+							}, 1500);
+						}
+					}, idx * 1500);
+				});
+		} else {
+			setMessages(currentDialog.messages);
+			setCompleted(true);
+		}
+	}, [slug, status]);
 
-  const container = {
-    show: {
-      transition: {
-        staggerChildren: 1,
-      },
-    },
-  };
+	const containerMotion = {
+		show: {
+			transition: {
+				staggerChildren: 1,
+			},
+		},
+	};
 
-  const item = {
-    hidden: { scale: 0, opacity: 0 },
-    show: { opacity: 1, scale: 1 },
-  };
+	const itemMotion = {
+		hidden: { x: -100, scale: 0, opacity: 0 },
+		show: { x: 0, opacity: 1, scale: 1 },
+	};
 
-  return (
-    <div className="Chat">
-      <div className="Window">
-        <img className="Head" src={Head} alt="Head" />
-        <motion.div
-          variants={container}
-          initial="hidden"
-          animate="show"
-          className="MessageContainer"
-        >
-          {messages &&
-            messages.map((message, idx) => {
-              return (
-                <motion.div key={`message-${idx}`} variants={item}>
-                  <Message status={status} data={message} />
-                </motion.div>
-              );
-            })}
-        </motion.div>
-        {!completed && <Typing />}
-        <motion.div
-          variants={container}
-          initial="hidden"
-          animate="show"
-          className="AnswerContainer"
-        >
-          {completed &&
-            currentDialog.answers &&
-            currentDialog.answers.map((message, idx) => {
-              return (
-                <motion.div key={`answer-${idx}`} variants={item}>
-                  <Answer data={message} />
-                </motion.div>
-              );
-            })}
-        </motion.div>
-      </div>
-    </div>
-  );
+	return (
+		<div className="Chat">
+			<motion.div
+				variants={containerMotion}
+				initial="hidden"
+				animate="show"
+				className="Window"
+			>
+				<img className="Head" src={Head} alt="Head" />
+				<div className="MessageContainer">
+					{messages &&
+						messages.map((message, idx) => {
+							return (
+								<motion.div key={`message-${idx}`} variants={itemMotion}>
+									<Message status={status} message={message} />
+								</motion.div>
+							);
+						})}
+				</div>
+				{!completed && <Typing />}
+				<div className="AnswerContainer">
+					{completed &&
+						currentDialog.answers &&
+						currentDialog.answers.map((message, idx) => {
+							return (
+								<motion.div key={`answer-${idx}`} variants={itemMotion}>
+									<Answer data={message} />
+								</motion.div>
+							);
+						})}
+				</div>
+			</motion.div>
+		</div>
+	);
 }
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,11 +1,12 @@
-import React from "react";
-import "./Footer.scss";
-import { Logo } from "../../images";
+import React from 'react';
+import './Footer.scss';
+import { Logo } from '../../images';
+
 export default function Footer() {
-  return (
-    <footer className="Footer">
-      <Logo color="#FFF" />
-      <div>Made with ❤ by Magnus Westhofen </div>
-    </footer>
-  );
+	return (
+		<footer className="Footer">
+			<Logo color="#FFF" />
+			<div>Made with ❤ by Magnus</div>
+		</footer>
+	);
 }
