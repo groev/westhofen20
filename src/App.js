f4d983f1daf86f8c2c82f4530c5606b591648c65
Magnus Westhofen <magnuswesthofen@icloud.com>
2020-09-22 09:24:33 +0200

    htaccess & layouteffect

--- src/App.js
+++ src/App.js
@@ -1,42 +1,42 @@
-import React, { useEffect, useState } from "react";
-import "./styles/index.scss";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import React, { useLayoutEffect, useState } from 'react';
+import './styles/index.scss';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import {
-  Header,
-  Container,
-  Chat,
-  Portfolio,
-  Footer,
-  Loader,
-} from "./components";
+	Header,
+	Container,
+	Chat,
+	Portfolio,
+	Footer,
+	Loader,
+} from './components';
 
 function App() {
-  const [isLoading, setIsLoading] = useState(true);
-  useEffect(() => {
-    setTimeout(() => {
-      setIsLoading(false);
-    }, 2500);
-  }, []);
-  return (
-    <BrowserRouter>
-      {!isLoading ? (
-        <div className="App">
-          <Header />
-          <Container>
-            <Switch>
-              <Route path="/portfolio" component={Portfolio} />
-              <Route path="/chat/:slug/:status" component={Chat} />
-              <Route path="/chat/:slug/" component={Chat} />
-              <Chat />
-            </Switch>
-          </Container>
-          <Footer />
-        </div>
-      ) : (
-        <Loader />
-      )}
-    </BrowserRouter>
-  );
+	const [isLoading, setIsLoading] = useState(true);
+	useLayoutEffect(() => {
+		setTimeout(() => {
+			setIsLoading(false);
+		}, 2500);
+	}, []);
+	return (
+		<BrowserRouter>
+			{!isLoading ? (
+				<div className="App">
+					<Header />
+					<Container>
+						<Switch>
+							<Route path="/portfolio" component={Portfolio} />
+							<Route path="/chat/:slug/:status" component={Chat} />
+							<Route path="/chat/:slug/" component={Chat} />
+							<Chat />
+						</Switch>
+					</Container>
+					<Footer />
+				</div>
+			) : (
+				<Loader />
+			)}
+		</BrowserRouter>
+	);
 }
 
 export default App;
