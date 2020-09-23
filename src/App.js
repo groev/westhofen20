de3ce791a8e61c83b30614870ca4d0fc858383d2
Magnus Westhofen <m.westhofen@12-05.de>
2020-09-23 20:06:28 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,42 +1,44 @@
-import React, { useLayoutEffect, useState } from 'react';
-import './styles/index.scss';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import React, { useLayoutEffect, useState } from "react";
+import "./styles/index.scss";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import {
-	Header,
-	Container,
-	Chat,
-	Portfolio,
-	Footer,
-	Loader,
-} from './components';
+  Header,
+  Container,
+  Chat,
+  Portfolio,
+  Footer,
+  Loader,
+} from "./components";
 
 function App() {
-	const [isLoading, setIsLoading] = useState(true);
-	useLayoutEffect(() => {
-		setTimeout(() => {
-			setIsLoading(false);
-		}, 2500);
-	}, []);
-	return (
-		<BrowserRouter>
-			{!isLoading ? (
-				<div className="App">
-					<Header />
-					<Container>
-						<Switch>
-							<Route path="/portfolio" component={Portfolio} />
-							<Route path="/chat/:slug/:status" component={Chat} />
-							<Route path="/chat/:slug/" component={Chat} />
-							<Chat />
-						</Switch>
-					</Container>
-					<Footer />
-				</div>
-			) : (
-				<Loader />
-			)}
-		</BrowserRouter>
-	);
+  const [isLoading, setIsLoading] = useState(true);
+  useLayoutEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2500);
+  }, []);
+  return (
+    <BrowserRouter>
+      <div className="App">
+        {!isLoading && (
+          <React.Fragment>
+            {" "}
+            <Header />
+            <Container>
+              <Switch>
+                <Route path="/portfolio" component={Portfolio} />
+                <Route path="/chat/:slug/:status" component={Chat} />
+                <Route path="/chat/:slug/" component={Chat} />
+                <Chat />
+              </Switch>
+            </Container>
+            <Footer />
+          </React.Fragment>
+        )}
+      </div>
+      <Loader isLoading={isLoading} />
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/components/Loader/index.js
+++ src/components/Loader/index.js
@@ -2,36 +2,37 @@ import React from "react";
 import "./Loader.scss";
 import { motion, AnimatePresence } from "framer-motion";
 
-export default function Loader({ loading }) {
+export default function Loader({ isLoading }) {
   return (
     <AnimatePresence>
-      <motion.div
-        className="Loader"
-        initial={{ opacity: 1 }}
-        visible={{ opacity: 1 }}
-        exit={{ opacity: 0 }}
-        transition={{
-          duration: 1,
-          ease: "easeInOut",
-        }}
-      >
-        <svg
-          version="1.1"
-          xmlns="http://www.w3.org/2000/svg"
-          x="0px"
-          y="0px"
-          width="258.8px"
-          height="164.2px"
-          viewBox="0 0 258.8 164.2"
+      {isLoading && (
+        <motion.div
+          className="Loader"
+          initial={{ opacity: 1 }}
+          visible={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{
+            duration: 1,
+            ease: "easeInOut",
+          }}
         >
-          <motion.path
-            initial={{ fill: "#FFF" }}
-            animate={{ fill: "#FFF" }}
-            transition={{
-              duration: 2,
-              ease: "easeInOut",
-            }}
-            d="M248.4,28.2c-4,7.8-8,15.5-12.1,23.3c-4.1,7.7-8.1,15.5-12.3,23.2c-4.1,7.7-8.2,15.4-12.4,23.1l-12.5,23l-2.9,5.4l-2.8-5.6
+          <svg
+            version="1.1"
+            xmlns="http://www.w3.org/2000/svg"
+            x="0px"
+            y="0px"
+            width="258.8px"
+            height="164.2px"
+            viewBox="0 0 258.8 164.2"
+          >
+            <motion.path
+              initial={{ fill: "#FFF" }}
+              animate={{ fill: "#FFF" }}
+              transition={{
+                duration: 2,
+                ease: "easeInOut",
+              }}
+              d="M248.4,28.2c-4,7.8-8,15.5-12.1,23.3c-4.1,7.7-8.1,15.5-12.3,23.2c-4.1,7.7-8.2,15.4-12.4,23.1l-12.5,23l-2.9,5.4l-2.8-5.6
           c-2.5-4.9-4.9-9.7-7.3-14.6l-7.2-14.6l-7.2-14.6c-2.4-4.9-4.8-9.8-7.2-14.7l7.6,0.3L156.4,90l-7.9,13.8l-7.9,13.8l-6.1,10.5
           l-2.8-11.8L122,75.9c-1.6-6.7-3.2-13.5-4.7-20.3l-4.7-20.3l9.1,1.1l-12.6,24c-4.2,8-8.5,15.9-12.7,23.9l-12.7,23.9
           c-4.2,8-8.6,15.9-12.9,23.8l-5,9.2l-2.3-10.1l-4.2-18.8c-1.4-6.3-2.8-12.6-4.1-18.8c-2.7-12.6-5.5-25.1-8.1-37.7l4,0.9
@@ -40,25 +41,26 @@ export default function Loader({ loading }) {
           c4.1-8,8.1-16.1,12.3-24.1L113,32l6-11.7l3.1,12.8l4.8,20.2c1.6,6.7,3.2,13.5,4.8,20.2l9.4,40.5l-8.9-1.3l8.1-13.7l8.1-13.7
           l16.3-27.3l4.2-7l3.4,7.4c2.3,4.9,4.6,9.9,6.8,14.8l6.8,14.9l6.8,14.9c2.3,4.9,4.5,9.9,6.7,14.9l-5.7-0.2l13.5-22.5
           c4.5-7.5,9.1-14.9,13.6-22.4c4.5-7.5,9.1-14.9,13.7-22.3C239,43,243.7,35.6,248.4,28.2z"
-          />
-          <motion.path
-            strokeIinejoin="round"
-            initial={{ pathLength: 1 }}
-            animate={{ pathLength: 0 }}
-            transition={{
-              duration: 2,
-              ease: "easeInOut",
-            }}
-            style={{
-              stroke: "#000",
-              fill: "transparent",
-              strokeWidth: 20,
-              strokeMiterlimit: 20,
-            }}
-            d="M248.4,28.2 196.4,119.2 168.4,60.2 136.4,115.2 117.4,34.2 67.4,130.2 49.4,55.2 9.4,105.2"
-          />
-        </svg>
-      </motion.div>
+            />
+            <motion.path
+              strokeIinejoin="round"
+              initial={{ pathLength: 1 }}
+              animate={{ pathLength: 0 }}
+              transition={{
+                duration: 2,
+                ease: "easeInOut",
+              }}
+              style={{
+                stroke: "#000",
+                fill: "transparent",
+                strokeWidth: 20,
+                strokeMiterlimit: 20,
+              }}
+              d="M248.4,28.2 196.4,119.2 168.4,60.2 136.4,115.2 117.4,34.2 67.4,130.2 49.4,55.2 9.4,105.2"
+            />
+          </svg>
+        </motion.div>
+      )}
     </AnimatePresence>
   );
 }
