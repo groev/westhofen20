74a526f5d0e0c0befb8c8ddccf84a10e71d1a823
Magnus Westhofen <m.westhofen@12-05.de>
2022-08-03 11:55:30 +0200

    test

--- src/images/index.js
+++ src/images/index.js
@@ -1,7 +1,7 @@
 import Logo from "./Logo";
 import Github from "./Github";
 import LinkedIn from "./LinkedIn";
-import Head from "./head.svg";
+import Head from "./head.jpg";
 import Chat from "./chat.svg";
 
 export { Logo, Github, LinkedIn, Head, Chat };
